6c6586d95b7a2c33d473baede41dd3a7869db4e0
Dalia <dc@dalia.cc>
2025-02-21 12:22:04 -0700

    split headers and definitions

---
+++ src/widgets.cpp
@@ -0,0 +1,63 @@
+#include "widgets.hpp"
+
+namespace dalia {
+void
+CharacterDisplay::drawLayer(const DrawArgs& args, int layer)
+{
+  if (layer == 1) {
+    // std::cout << "drawing CharacterDisplay\n";
+    std::shared_ptr<rack::Font> font = APP->window->loadFont(fontPath);
+    if (font) {
+      nvgFontFaceId(args.vg, font->handle);
+      nvgFontSize(args.vg, 16);
+      nvgFillColor(args.vg, nvgRGBA(255, 255, 255, 20));
+      nvgTextAlign(args.vg, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE);
+      nvgText(args.vg,
+              0.0,
+              0.0,
+              std::string(text.length(), normalChar).c_str(),
+              NULL);
+      nvgFillColor(args.vg, nvgRGBA(255, 255, 255, 255));
+      nvgText(args.vg, 0.0, 0.0, text.c_str(), NULL);
+    }
+  }
+  rack::Widget::drawLayer(args, layer);
+}
+
+std::string
+FloatSegmentDisplay::getText()
+{
+  std::stringstream text;
+  if (length <= 0) {
+    return text.str();
+  }
+  float value = std::abs(getValue());
+  int lg10 = std::log10(value);
+  int precision = length - 2;
+  if (lg10 >= length) {
+    value = (int)value % (int)std::pow(10, length);
+    lg10 = std::log10(value);
+  }
+  if (lg10 > 0) {
+    precision = std::max(precision - lg10, 0);
+  }
+
+  text << std::fixed << std::setw(length - 2) << std::setprecision(precision)
+       << value;
+  return text.str();
+}
+
+void
+FloatSegmentDisplay::draw(const DrawArgs& args)
+{
+  float value = getValue(); // std::abs(getValue());
+  if (value != prevValue) {
+    prevValue = value;
+
+    displayWidget->text = getText();
+    // fb->setDirty();
+  }
+  rack::Widget::draw(args);
+}
+
+} // namespace dalia
--- src/widgets.hpp
+++ src/widgets.hpp
@@ -3,9 +3,9 @@
 // #include <iostream>
 #include <sstream>
 
-#include "functional"
 #include "plugin.hpp"
 #include "rack.hpp"
+#include <functional>
 
 namespace dalia {
 struct CKSSHorizontal : rack::app::SvgSwitch
@@ -27,7 +27,6 @@ private:
 
 public:
   std::string fontPath;
-  // int length = 6; // including the decimal point
   char normalChar = '@';
   std::string text;
 
@@ -37,27 +36,7 @@ public:
                                    "res/9-segment-black/9-segment-black.ttf");
   }
 
-  void drawLayer(const DrawArgs& args, int layer) override
-  {
-    if (layer == 1) {
-      // std::cout << "drawing CharacterDisplay\n";
-      std::shared_ptr<rack::Font> font = APP->window->loadFont(fontPath);
-      if (font) {
-        nvgFontFaceId(args.vg, font->handle);
-        nvgFontSize(args.vg, 16);
-        nvgFillColor(args.vg, nvgRGBA(255, 255, 255, 20));
-        nvgTextAlign(args.vg, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE);
-        nvgText(args.vg,
-                0.0,
-                0.0,
-                std::string(text.length(), normalChar).c_str(),
-                NULL);
-        nvgFillColor(args.vg, nvgRGBA(255, 255, 255, 255));
-        nvgText(args.vg, 0.0, 0.0, text.c_str(), NULL);
-      }
-    }
-    rack::Widget::drawLayer(args, layer);
-  }
+  void drawLayer(const DrawArgs& args, int layer) override;
 };
 
 struct FloatSegmentDisplay : rack::Widget
@@ -82,39 +61,8 @@ public:
     // fb->addChild(displayWidget);
   }
 
-  std::string getText()
-  {
-    std::stringstream text;
-    if (length <= 0) {
-      return text.str();
-    }
-    float value = std::abs(getValue());
-    int lg10 = std::log10(value);
-    int precision = length - 2;
-    if (lg10 >= length) {
-      value = (int)value % (int)std::pow(10, length);
-      lg10 = std::log10(value);
-    }
-    if (lg10 > 0) {
-      precision = std::max(precision - lg10, 0);
-    }
-
-    text << std::fixed << std::setw(length - 2) << std::setprecision(precision)
-         << value;
-    return text.str();
-  }
-
-  void draw(const DrawArgs& args) override
-  {
-    float value = getValue(); // std::abs(getValue());
-    if (value != prevValue) {
-      prevValue = value;
-
-      displayWidget->text = getText();
-      // fb->setDirty();
-    }
-    rack::Widget::draw(args);
-  }
+  std::string getText();
+  void draw(const DrawArgs& args) override;
 };
 
 } // namespace dalia
